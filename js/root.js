ca903252f561781dd6adf80165f4e20eaf580e3a
Stefano Tosatto <stefanotosatto.work@gmail.com>
2021-03-30 16:01:00 +0200

    ok

--- js/root.js
+++ js/root.js
@@ -2,15 +2,25 @@ var app = new Vue({
     el: "#root",
 
     data: {
+        albums: []
         
     },
 
     mounted() {
-
+        axios
+            .get("https://flynn.boolean.careers/exercises/api/array/music")
+            .then((response) => {
+                for(let i = 0; i < response.data.response.length; i++){
+                    let result = response.data.response[i];
+                    console.log(result);
+                    this.albums.push(result);
+                };
+            });
+        console.log(this.albums);
+        
     },
 
     methods: {
-        
 
     }
 });
